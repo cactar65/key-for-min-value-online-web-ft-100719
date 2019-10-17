ce0fc1fb0d94c9d81505891e6b2226504d1cb37a
Nicholas Walton <cactar65@gmail.com>
2019-10-17 16:11:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+  name_hash.each do |key, value|
+    
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
+
 end
